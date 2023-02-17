9251088aa509663510fd95d202550aeeefdc4f6b
Timur Tarazov <johnsyplay@yandex.ru>
2023-02-17 16:30:00 +0500

    header added

---
+++ src/components/navbar.tsx
@@ -0,0 +1,25 @@
+export function Navbar() {
+    return (
+        <header
+        className="
+        h-auto
+        bg-black/70
+        py-[0.5rem]
+        fixed">
+            <div
+            className="
+            container
+            mx-auto">
+                <h1
+                className="
+                font-serif
+                text-2xl
+                text-yellow-500
+                leading-5">
+                    Золотой<br />
+                    ключик
+                </h1>
+            </div>
+        </header>
+    );
+}
