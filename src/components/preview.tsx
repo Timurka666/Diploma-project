d785f642573a877aba48c2cc6e7b34a5f3b08e63
Timur Tarazov <johnsyplay@yandex.ru>
2023-02-26 20:31:48 +0500

    responsive review done

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -14,16 +14,24 @@ export function Preview() {
             bg-black/70
             w-[100%]
             h-[100%]
-            pt-[10rem]">
+            pt-[45%]
+            lg:pt-[10rem]
+            md:pt-[10rem]">
                 <div
                 className="
                 bg-white/0
                 border-solid
                 border-white
-                border-[0.5rem]
+                border-[0.3rem]
+                lg:border-[0.5rem]
+                md:border-[0.4rem]
                 mx-auto
-                h-[15rem]
-                w-[30rem]
+                w-[17rem]
+                h-[8rem]
+                lg:h-[15rem]
+                lg:w-[30rem]
+                md:w-[25rem]
+                md:h-[13rem]
                 py-[1rem]
                 px-[0.5rem]
                 after:content-['']
@@ -31,11 +39,15 @@ export function Preview() {
                 after:bg-white
                 after:w-[80%]
                 after:h-[1px]
-                after:mt-[2rem]
+                after:mt-[1rem]
+                lg:after:mt-[2rem]
+                md:after:mt-[3rem]
                 after:mx-auto">
                     <h1
                     className="
-                    text-3xl
+                    text-md
+                    lg:text-3xl
+                    md:text-xl
                     text-yellow-500
                     font-bold
                     text-center">
@@ -45,14 +57,18 @@ export function Preview() {
                     <img
                     src={goldenKey}
                     alt=""
-                    width="75"
                     className="
                     -rotate-90
-                    mx-auto" />
+                    mx-auto
+                    w-[30px]
+                    lg:w-[75px]
+                    md:w-[60px]" />
                 </div>
                 <p
                 className="
-                mt-[3rem]
+                mt-[2rem]
+                lg:mt-[3rem]
+                md:mt-[3rem]
                 text-center
                 text-white/60">Продажа и ремонт ювелирных изделий, а также изготовление ключей</p>
             </div>
